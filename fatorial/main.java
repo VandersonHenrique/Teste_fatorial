63be44812e4b2ccbda13075e3574591d9976f400
VandersonHenrique <132020975+VandersonHenrique@users.noreply.github.com>
2025-05-16 23:29:56 +0000

    adição do import na classe main

--- fatorial/main.java
+++ fatorial/main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class main{
     public static void main(String[] args){
-        int numero = 5;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um numero para se calcular o fatorial com esse numero não podendo ser negativo");
+        int numero = teclado.nextInt();
         long x = fatorial.fatorial(numero);
         if(x<0){
              System.out.println("Erro: número inválido");
